5e06bf6632f2d03d56f031824408ed0f54a172be
dilliswararaogalala <dilliswararaogalala@gmail.com>
2026-05-07 17:27:12 +0530

    Refactors the chance methods intersection and union. Removes min and max(normalize chance between 0-1)

--- src/com/bootcamp/tw/p2/Chance.java
+++ src/com/bootcamp/tw/p2/Chance.java
@@ -3,56 +3,42 @@ package com.bootcamp.tw.p2;
 import java.util.Objects;
 
 public class Chance {
-
     private final double chance;
-    private final double min;
-    private final double max;
 
-    public Chance(double chance, double min, double max  ) {
+    public Chance(double chance) {
         this.chance = chance;
-        this.min = min;
-        this.max = max;
     }
 
     public static Chance create(double chance) {
-        return new Chance(chance, 0, 1);
+        return new Chance(chance);
     }
 
     public static Chance create(double chance, double max) {
-        return new Chance(chance, 0, max);
-    }
-
-    public static Chance create(double chance,double min, double max) {
-        return new Chance(chance, min, max);
-    }
-
-    public double getChance() {
-        return this.chance;
+        return new Chance(chance / max);
     }
 
     public Chance not() {
-        return Chance.create(max - this.chance + min , min, max);
+        return Chance.create(1 - this.chance);
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Chance chance1 = (Chance) o;
-        return Double.compare(chance, chance1.chance) == 0 && Double.compare(min, chance1.min) == 0 && Double.compare(max, chance1.max) == 0;
+        return Double.compare(chance, chance1.chance) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(chance, min, max);
+        return Objects.hash(chance);
     }
 
+
     public Chance intersection(Chance chance2) {
-        return Chance.create(this.chance * chance2.chance, min, max);
+        return Chance.create(this.chance * chance2.chance);
     }
 
     public Chance union(Chance chance2) {
-        Chance notOfChance1 = this.not();
-        Chance notOfChance2 = chance2.not();
-        return Chance.create(notOfChance1.chance * notOfChance2.chance, min, max).not();
+        return Chance.create((this.chance + chance2.chance - this.intersection(chance2).chance));
     }
 }
--- test/com/bootcamp/tw/p2/ChanceTest.java
+++ test/com/bootcamp/tw/p2/ChanceTest.java
@@ -10,7 +10,7 @@ public class ChanceTest {
     @Description("Represent the chance of getting Tail")
     void getChanceWhenWeCalledChanceOf() {
         Chance gettingTail = Chance.create(0.5);
-        assertEquals(0.5, gettingTail.getChance());
+        assertEquals(new Chance(0.5), gettingTail);
     }
 
     @Test
@@ -18,36 +18,22 @@ public class ChanceTest {
     void getNotChanceWhenWeCalledGetNotChanceOf() {
         Chance gettingTail = Chance.create(0.2);
         Chance notGettingChance = gettingTail.not();
-        assertEquals(0.8, notGettingChance.getChance());
+        assertEquals( Chance.create(0.8), notGettingChance);
     }
 
     @Test
-    @Description("Represent the chance to get tail when we flip 2 coins")
-    void chanceToGetTailWhenWeFlippedTwoCoins() {
-        Chance gettingTail = Chance.create(0.25);
-        assertEquals(0.25, gettingTail.getChance());
-    }
-
-    @Test
-    @Description("Represent the chance of not getting Tail when we flip 2 coins")
+    @Description("Represent the chance of getting Tail in both coin when we flip 2 coins")
     void chanceToNotGetTailWhenWeFlippedTwoCoins() {
         Chance gettingTailInCoin1 = Chance.create(0.5);
         Chance gettingTailInCoin2 = Chance.create(0.5);
 
         Chance gettingTailInBothChance = gettingTailInCoin1.intersection(gettingTailInCoin2);
-
         assertEquals(Chance.create(0.25), gettingTailInBothChance);
     }
 
-    @Test
-    @Description("Represent the chance to get tail when we flip 2 coins")
-    void chanceToGetThreeWhenWeRollDice() {
-        Chance gettingThreeChanceInDice = Chance.create(1, 6);
-        assertEquals(new Chance(1, 0, 6), gettingThreeChanceInDice);
-    }
 
     @Test
-    @Description("Represent the chance of not getting Tail when we flip 2 coins")
+    @Description("Represent the chance of at least one Tail when we flip 2 coins")
     void chanceOfGettingTailOnBothCoins() {
         Chance gettingTailInCoin1 = Chance.create(0.5);
         Chance gettingTailInCoin2 = Chance.create(0.5);
@@ -56,6 +42,10 @@ public class ChanceTest {
 
         assertEquals(Chance.create(0.75), gettingTailInAnyOne);
     }
-
-
+    @Test
+    @Description("Represent the chance to get 3 when we roll dice")
+    void chanceToGetThreeWhenWeRollDice() {
+        Chance gettingThreeChanceInDice = Chance.create(1, 6);
+        assertEquals(new Chance((double) 1 /6), gettingThreeChanceInDice);
+    }
 }
