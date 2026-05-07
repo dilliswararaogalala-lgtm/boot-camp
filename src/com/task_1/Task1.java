e01affbd8800fd2baf3bbcd7219bd9eb672893a6
dilliswararaogalala <dilliswararaogalala@gmail.com>
2026-05-07 10:57:55 +0530

    Setups the java project and add simple test cases

---
+++ src/com/task_1/Task1.java
@@ -0,0 +1,11 @@
+package com.task_1;
+
+public class Task1 {
+    public Task1() {
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ test/com/task_1/Task1Test.java
@@ -0,0 +1,12 @@
+package com.task_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task1Test {
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+        Task1 task1 = new Task1();
+        assertEquals(1, 1);
+    }
+}
