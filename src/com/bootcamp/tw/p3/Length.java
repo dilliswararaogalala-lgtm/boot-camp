ac5fd47a285d725fc1eea787fb9181af96e4a0f4
dilliswararaogalala <dilliswararaogalala@gmail.com>
2026-05-08 12:19:10 +0530

    Modifies Length class and remove Unit type Dependency after storing

--- src/com/bootcamp/tw/p3/Length.java
+++ src/com/bootcamp/tw/p3/Length.java
@@ -3,18 +3,18 @@ import java.util.Objects;
 
 public class Length {
     private final Double unitValue;
-    private final Unit unit;
 
-    private Length(Double unitValue, Unit unit) {
-        this.unitValue = unitValue;
-        this.unit = unit;
+    private Length(Double unitValueInInch) {
+        this.unitValue = unitValueInInch;
     }
 
     private static Length createLength(Double unitValue, Unit unit) throws InvalidUnitValue {
         if(unitValue < 0) {
             throw  new InvalidUnitValue("Negative length Not allowed");
         }
-        return new Length(unitValue, unit);
+
+        double unitValueInInch = unitValue * unit.ratioToInch;
+        return new Length(unitValueInInch);
     }
 
     public static Length createFeet(double value) throws InvalidUnitValue {
@@ -33,21 +33,9 @@ public class Length {
         return createLength(value, Unit.MM);
     }
 
-
-    boolean compare(Length a) {
-        if (a.unit.equals(this.unit)) {
-            return this.equals(a);
-        }
-
-        double unitInInch = this.toInch();
-        double comparatorUnitsInch = a.toInch();
-        return unitInInch == comparatorUnitsInch;
+    boolean compare(Object a) {
+       return this.equals(a);
     }
-
-    private double toInch() {
-        return this.unit.ratioToInch * this.unitValue;
-    }
-
 
     @Override
     public boolean equals(Object o) {
@@ -58,7 +46,7 @@ public class Length {
 
     @Override
     public int hashCode() {
-        return Objects.hash(unitValue, unit);
+        return Objects.hash(unitValue);
     }
 }
 
--- src/com/bootcamp/tw/p3/Unit.java
+++ src/com/bootcamp/tw/p3/Unit.java
@@ -2,14 +2,13 @@ package com.bootcamp.tw.p3;
 
 public enum Unit {
     FEET(12),
-    CM(0.4), INCH(1), MM(0.04);
+    CM(0.4),
+    INCH(1),
+    MM(0.04);
 
     public final double ratioToInch;
 
     Unit(double ratioToInch) {
         this.ratioToInch = ratioToInch;
     }
-
-
-
 }
