b93210d241fb8a38f18fd4312f39cb405ed9151d
dilliswararaogalala <dilliswararaogalala@gmail.com>
2026-05-09 15:01:41 +0530

    Replaces the constructor of bag with factory method

--- src/com/bootcamp/tw/p5/Bag.java
+++ src/com/bootcamp/tw/p5/Bag.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 
 public class Bag {
     private final ArrayList<Ball> balls = new ArrayList<>();
+    private final int maxCapacity;
+
+    private Bag(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public static Bag createBag(int maxCapacity) {
+        return new Bag(maxCapacity);
+    }
 
     public boolean add(Ball ball) throws UnableToAddBallException {
-        if (balls.size() >= 12){
+        if (balls.size() >= maxCapacity){
             throw new UnableToAddBallException("Bag is full");
         }
         balls.add(ball);
--- test/com/bootcamp/tw/p5/BagTest.java
+++ test/com/bootcamp/tw/p5/BagTest.java
@@ -8,17 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BagTest {
     @Test
     void shouldAbleToAddBallInBag() throws UnableToAddBallException {
-        Bag bag = new Bag();
+        Bag bag = Bag.createBag(12);
         boolean isAdded = bag.add(new Ball());
         assertTrue(isAdded);
     }
 
     @Test
     void shouldThrowWhenBagIsFull() throws UnableToAddBallException {
-        Bag bag = new Bag();
-        for (int i = 0 ; i < 12; i++){
-            bag.add(new Ball());
-        }
+        Bag bag = Bag.createBag(1);
+        bag.add(new Ball());
         assertThrows(UnableToAddBallException.class, ()-> bag.add(new Ball()));
     }
 }
